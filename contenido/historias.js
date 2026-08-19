1ab583f351ec699e5acbfdce3bbea0f5824de4bd
benoitgero <167662521+benoitgero@users.noreply.github.com>
2026-08-19 15:37:30 -0300

    chnage order add text

--- contenido/historias.js
+++ contenido/historias.js
@@ -16,9 +16,9 @@
 
 export const HISTORIAS = [
   { id: "jap",    tab: "JAP",    titulo: "Jóvenes a Programar",             imagen: "assets/historias/jap.jpg" },
-  { id: "forge",  tab: "Forge",  titulo: "Fundación Forge", imagen: "assets/historias/forge.jpg" },
   { id: "ces",    tab: "CES",    titulo: "Centro de Ensayos de Software",             imagen: "assets/historias/ces.jpg" },
+  { id: "switch", tab: "Switch", titulo: "Switch Software Solutions",          imagen: "assets/historias/switch.jpg" },
+  { id: "forge",  tab: "Forge",  titulo: "Fundación Forge", imagen: "assets/historias/forge.jpg" },
   { id: "fadu",   tab: "FADU",   titulo: "Facultad de Arquitectura, Diseño y Urbanismo",            imagen: "assets/historias/fadu.jpg" },
-  { id: "tcs",    tab: "TCS",    titulo: "TCS",             imagen: "assets/historias/tcs.jpg" },
-  { id: "switch", tab: "Switch", titulo: "Switch",          imagen: "assets/historias/switch.jpg" },
+  { id: "tcs",    tab: "TCS",    titulo: "Tata Consultancy Services",             imagen: "assets/historias/tcs.jpg" },
 ];
